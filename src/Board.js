262317bb79a4b66fe79f658546c51ad2839b7933
tool <tool.kamil@gmail.com>
2019-01-21 21:48:51 +0100

    all conditions for moving

--- src/Board.js
+++ src/Board.js
@@ -9,40 +9,46 @@ export default class Board extends Component{
       player: {
         x: 0,
         y: 0
-      }
+      },
+      boxes: [
+        [80,80], [120,80],[160,0]
+      ]
     };
 
     this.arrowCodes = {left: 37, up: 38, right: 39, down: 40}; /* TODO przerobić na const albo wynieść do utils */
-    this.boxes = [[80,80], [160,0]];
+    this.walls = [[40,0], [200,0]];
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.getIntendedPosition = this.getIntendedPosition.bind(this);
+    this.checkIfWallIsNear = this.checkIfWallIsNear.bind(this);
     this.checkIfEndOfBoard = this.checkIfEndOfBoard.bind(this);
     this.checkIfBoxIsNear = this.checkIfBoxIsNear.bind(this);
+    this.getIndexOfNearBox = this.getIndexOfNearBox.bind(this);
+    this.checkIfBoxIsBlocked = this.checkIfBoxIsBlocked.bind(this);
+    this.movePlayer = this.movePlayer.bind(this);
+    this.movePlayerWithBox = this.movePlayerWithBox.bind(this);
   }
 
   componentDidMount(){
     window.addEventListener('keyup', this.handleKeyDown);
   }
 
-  getIntendedPosition(keyCode){
-    let intendedPosition = {...this.state.player};
-
+  getIntendedPosition(position, keyCode){
     switch(keyCode){
       case this.arrowCodes.right:
-        intendedPosition.x = this.state.player.x + 40;
+        position.x += 40;
         break;
       case this.arrowCodes.up:
-        intendedPosition.y = this.state.player.y - 40;
+        position.y -= 40;
         break;
       case this.arrowCodes.down:
-        intendedPosition.y = this.state.player.y + 40;
+        position.y += 40;
         break;
       case this.arrowCodes.left:
-        intendedPosition.x = this.state.player.x - 40;
+        position.x -= 40;
         break;
     }
 
-    return intendedPosition;
+    return position;
   }
 
   checkIfEndOfBoard(position){
@@ -55,6 +61,15 @@ export default class Board extends Component{
     return false;
   }
 
+  checkIfWallIsNear(position, walls){
+    let result = walls.some(function(wall){
+      if(wall[0] == position.x && wall[1] == position.y){
+        return true;
+      }
+    });
+    return result;
+  }
+
   checkIfBoxIsNear(position, boxes){
     let result = boxes.some(function(box){
       if(box[0] == position.x && box[1] == position.y){
@@ -62,6 +77,46 @@ export default class Board extends Component{
       }
     });
     return result;
+  }
+
+  checkIfBoxIsBlocked(position, boxes, walls){
+    if(this.checkIfEndOfBoard(position)){
+      return true;
+    }
+    if(this.checkIfWallIsNear(position, walls)){
+      return true;
+    }
+    if(this.checkIfBoxIsNear(position, boxes)){
+      return true;
+    }
+    
+    return false;
+  }
+
+  getIndexOfNearBox(position, boxes){
+    let indexOfNearBox = null;
+    boxes.some(function(box, index) {
+      if(box[0] == position.x && box[1] == position.y){
+        indexOfNearBox = index;
+        return true;
+      }
+    })
+    return indexOfNearBox;
+  }
+
+  movePlayer(position){
+    this.setState({
+      player: position
+    });
+  }
+
+  movePlayerWithBox(playerPosition, boxPosition, boxIndex){
+    let cloneState = {...this.state};
+    cloneState.player = playerPosition;
+    cloneState.boxes[boxIndex][0] = boxPosition.x;
+    cloneState.boxes[boxIndex][1] = boxPosition.y;
+
+    this.setState({...cloneState});
   }
 
   handleKeyDown(e){
@@ -77,25 +132,44 @@ export default class Board extends Component{
       return;
     }
 
-    let intendedPosition = this.getIntendedPosition(e.keyCode)
+    let intendedPlayerPosition = this.getIntendedPosition({...this.state.player}, e.keyCode)
 
-    if(this.checkIfEndOfBoard(intendedPosition)){
+    if(this.checkIfEndOfBoard(intendedPlayerPosition)){
       return;
     }
-    if(this.checkIfBoxIsNear(intendedPosition, this.boxes)){
+    if(this.checkIfWallIsNear(intendedPlayerPosition, this.walls)){
       return;
     }
-    
-    this.setState({
-      player: intendedPosition
-    });
+
+    let indexOfNearBox = this.getIndexOfNearBox(intendedPlayerPosition, this.state.boxes);
+    if(indexOfNearBox != null){
+      let intendedBoxPosition = this.getIntendedPosition({...intendedPlayerPosition}, e.keyCode)
+      if(this.checkIfBoxIsBlocked(intendedBoxPosition, this.state.boxes, this.walls)){
+        return;
+      }
+      else{
+        this.movePlayerWithBox(intendedPlayerPosition, intendedBoxPosition, indexOfNearBox);
+        console.log(this.state);
+        return;
+      }
+    }
+
+    this.movePlayer(intendedPlayerPosition);
+    console.log(this.state);
   }
 
   render(){
+    let boxes = this.state.boxes.map((box, i) =>
+      <div key={i.toString()} className="box" style={{left: box[0], top: box[1]}}></div>
+    );
+    let walls = this.walls.map((wall, i) =>
+      <div key={i.toString()} className="wall" style={{left: wall[0], top: wall[1]}}></div>
+    );
+    
     return(
       <div className="board">
-        <div className="box" style={{left:80, top: 80}}></div>
-        <div className="box" style={{left:160, top: 0}}></div>
+        {boxes}
+        {walls}
         <PlayerBox player={this.state.player} />
       </div>
     );
